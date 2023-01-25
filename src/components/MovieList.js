c58fa0bac67d7fc1946636e77d2ae8845bb91ac0
Sawyer <sawyer.welter@gmail.com>
2023-01-24 22:17:33 -0600

    Task 1 and 2 completed: connected MovieList, Movie, and MovieHeader to the reducer

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
-    const movies = [];
-
     return (
         <div className="col">
             <table className="table table-striped table-hover">
@@ -21,14 +20,31 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
             
-            <MovieFooter totalMovies={movies.length}/>
+            <MovieFooter totalMovies={props.movies.length}/>
         </div>
     );
 }
 
-export default MovieList;
+const myMovies = [
+    {
+      id: 0,
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      metascore: 100,
+      genre: "Drama",
+      description: "War hero Michael is the prodigal son of aging but fearsome crime patriarch Don Vito Corleone. When Michael returns home only to be thrust into an all-too-familiar world of hitmen, corrupt cops, and simmering mafia rivalries, he is forced to choose between his own path and the Corleone family legacy."
+    }]
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        movies: state.movies
+    }
+}
+
+export default connect(mapStateToProps, {})(MovieList);
